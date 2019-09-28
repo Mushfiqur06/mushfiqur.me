8fc1d215456b1ff00440dbec7e1dc070ecc3ce41
mushfiqur06 <mushfiqur06@gmail.com>
2019-09-28 13:28:43 +0600

    Slider Section Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 
-import Haders from './components/Header'
+import Haders from './components/Header';
+import Slider from './components/Slider'
 
 function App() {
   return (
     <div className="App">
       <Haders></Haders>
+      <div className="slider-section">
+        <Slider />
+      </div>
     </div>
   );
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {Container} from 'react-bootstrap';
+import ArrowDonwImg from './../arrow-down.svg'
+
+class Slider extends Component {
+    render() { 
+        return (
+            <div>
+                <Container>
+                    <div className="hero-section">
+                        <div className="hero-content">
+                            <span className="hero-intro-greeting">Hello, I am</span>
+                            <h1>Mushfiqur Rahman niloy</h1>
+                            <h4>Senior Front End Developer <span>At Zenix Technology</span></h4>
+                            <a className="hireBtn" href="#">hire Me</a>
+                            <a className="resumeBtn" href="#">Resume</a>
+                        </div>
+                    </div>
+                </Container>
+
+                <div className="next-section-link solitude-bg text-center">
+                    <span className="scroll-to-target">
+                        <img src={ArrowDonwImg} alt="" />
+                    </span>
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default Slider;
