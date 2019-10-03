b6932ef3d1daafda79fe33d9d30812195e24302c
Mushfiqur Rahman <mushfiqur06@gmail.com>
2019-10-03 18:04:10 +0600

    JobExperience section done

--- src/components/JobExperience.js
+++ src/components/JobExperience.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react';
 import PageSectionTitle from './PageSectionTitle';
 import {Container, Row, Col, Image} from 'react-bootstrap';
-import TrendThemeLogo from './../trendytheme.png'
+import TrendThemeLogo from './../trendytheme.png';
+import ThemeOnLabLogo from './../themeonlab.jpg';
+import ZenixNowLogo from './../zenixnow.png'
 
 class JobExperience extends Component {
 
     state = {
         jobExperienceData: [
             {
-                companyName: TrendThemeLogo,
-                position: 'Front-End Developer',
+                companyName: ZenixNowLogo,
+                position: 'Senior Front-End Developer',
                 joinDate: 'February 2016',
                 leaveDate: 'March 2017',
-                info: 'They make <i>premium WP and HTML theme</i> for <b>Themeforest</b> and they have their <b>own market</b> place to sell their product here.'
+                info: 'Zenix Technologies is a freelancing based company. I am a team leader of Front-End team. We have an awesome Back-End team. So we works together.',
+                technology: ['HTML', 'CSS', 'CSS3', 'Javascript', 'Gulp', 'ReactJS', 'VueJS', 'NuxtJS', 'GraphQL' ]
+            },
+            {
+                companyName: ThemeOnLabLogo,
+                position: 'Front-End Developer',
+                joinDate: 'April 2017',
+                leaveDate: 'December 2018',
+                info: 'They make WP & HTML theme for client and Themeforest. So i was a Front-End Developer.',
+                technology: ['HTML', 'CSS', 'CSS3', 'SASS', 'Javascript', 'Jquery', 'PSD to HTML', 'WordPress Theme Customize']
             },
             {
                 companyName: TrendThemeLogo,
                 position: 'Front-End Developer',
                 joinDate: 'February 2016',
                 leaveDate: 'March 2017',
-                info: 'They make <i>premium WP and HTML theme</i> for <b>Themeforest</b> and they have their <b>own market</b> place to sell their product here.'
+                info: 'They make premium WP and HTML theme for Themeforest and they have their own market place to sell their product here.',
+                technology: ['HTML', 'CSS', 'CSS3', 'Javascript', 'Jquery', 'PSD to HTML']
             },
-            {
-                companyName: TrendThemeLogo,
-                position: 'Front-End Developer',
-                joinDate: 'February 2016',
-                leaveDate: 'March 2017',
-                info: 'They make <i>premium WP and HTML theme</i> for <b>Themeforest</b> and they have their <b>own market</b> place to sell their product here.'
-            }
+            
+            
         ]
     }
 
@@ -53,6 +60,11 @@ class JobExperience extends Component {
                                         </div>
                                         <div className="jobExperienceWrapper__info">
                                             <p>{data.info}</p>
+                                        </div>
+                                        <div className="jobExperienceWrapper__techList">
+                                            <p><b>Technology:</b> {data.technology.map((tech, index) => {
+                                                return <span key={index}>{tech},</span>
+                                            })}</p>
                                         </div>
                                     </div>
                                 </Col>
