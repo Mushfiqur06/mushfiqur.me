cb912e928e07563ee91bcf7665dab7b229c82b33
Mushfiqur Rahman <mushfiqur06@gmail.com>
2019-10-06 20:00:43 +0600

    Contat component done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Form} from 'react-bootstrap'
 
 class Contact extends Component {
     state = {  }
     render() { 
+        
         return (
             <Container>
-                <Row>
-                    <Col md={12}>
-                        <h1>Hello from Contact Component</h1>
-                    </Col>
-                </Row>
+                <Form className="contactFormWrapper">
+                    <Row>
+                        <Col md={4}>
+                            <Form.Group controlId="validationCustom01">
+                                <Form.Label>Your Name</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="Your Name"
+                                    defaultValue="Mushfiqur Niloy"
+                                />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+
+                        <Col md={4}>
+                            <Form.Group controlId="validationCustom02">
+                                <Form.Label>Your Email</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="Email"
+                                    placeholder="Your Email"
+                                    defaultValue="message@example.com"
+                                />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+
+                        <Col md={4}>
+                            <Form.Group controlId="validationCustom03">
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="Subject"
+                                    defaultValue="Web Application"
+                                />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+
+                        <Col md={12}>
+                            <Form.Group controlId="validationCustom03">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control
+                                    className="textarea"
+                                    as="textarea" 
+                                    rows="5"
+                                    required
+                                    type="text"
+                                    placeholder="Message"
+                                    defaultValue="Tell me your plan or anything"
+                                />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+
+                        <div className="submitBtn">
+                            <button>Send Message</button>
+                        </div>
+                        
+                    </Row>
+                </Form>
             </Container>
         );
     }
