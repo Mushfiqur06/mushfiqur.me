82476a71c75d81d6e42f8840cc66192cf8e961d9
Mushfiqur Rahman <mushfiqur06@gmail.com>
2019-10-06 16:03:09 +0600

    Create Skill Component

---
+++ src/components/Skill.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Skill extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default Skill;
