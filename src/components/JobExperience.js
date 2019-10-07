5842f0510ef00f1a8e69fc26bd0bf6b950422251
Mushfiqur Rahman <mushfiqur06@gmail.com>
2019-10-07 16:04:35 +0600

    Mobile Responsive done

--- src/components/JobExperience.js
+++ src/components/JobExperience.js
@@ -46,7 +46,7 @@ class JobExperience extends Component {
                     <Row>
                         {this.state.jobExperienceData.map((data, index) => {
                             return(
-                                <Col md={6} key={index}>
+                                <Col lg={6} md={12} key={index}>
                                     <div className="jobExperienceWrapper">
                                         <div className="jobExperienceWrapper__header">
                                             <div className="jobExperienceWrapper__header--companyLogo">
